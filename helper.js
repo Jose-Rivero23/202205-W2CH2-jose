c089553b90b48e4597dbeca5117e14fe2239bcd9
Jose Rivero <josearmandorr95@gmail.com>
2022-05-17 19:12:38 +0200

    Add/test-functions-length-push-pop

--- helper.js
+++ helper.js
@@ -1,3 +1,12 @@
-export function add(num1 = 0, num2 = 0) {
-    return num1 + num2;
+export function arrayLength(array) {
+    return array.length;
+}
+export function push(array, item) {
+    array[arrayLength(array)] = item;
+    return array.length;
+}
+export function pop(array) {
+    let item = array[array.length - 1];
+    array.length = array.length - 1;
+    return item;
 }
--- helper.test.js
+++ helper.test.js
@@ -1,8 +1,47 @@
-import { add } from './helper.js';
+import { arrayLength, pop, push } from './helper.js';
 
-describe('function add', () => {
-    test('', () => {
-        expect(true).toBe(true);
-        expect(add(1, 2)).toBe(3);
+describe('function arrayLength []', () => {
+    test('When receive []', () => {
+        const param1 = arrayLength([]);
+        expect(param1).toBe(0);
+    });
+    describe('function arrayLength', () => {
+        test('When receive [ 7, 2 ,3]', () => {
+            const param1 = arrayLength([7, 2, 3]);
+
+            expect(param1).toBe(3);
+        });
+    });
+});
+describe('Given the function push', () => {
+    describe('When receive [], 1', () => {
+        test('they should be 1', () => {
+            const param1 = [];
+            const param2 = push([], 1);
+            expect(param2).toBe(1);
+        });
+    });
+    describe('When receive [], 1', () => {
+        test('Then should return manana, 1', () => {
+            const manzana = [];
+            push(manzana, 1);
+            expect(manzana).toContain(1);
+        });
+    });
+});
+
+describe('Given the function pop', () => {
+    describe('When receive [1 , 6 , 9]', () => {
+        test('they should be 3', () => {
+            const param2 = pop([1, 6, 9]);
+            expect(param2).toBe(9);
+        });
+    });
+    describe('When receive [1 , 6, 9]', () => {
+        test('Then should return manzana, 2', () => {
+            let manzana = [1, 6, 9];
+            pop(manzana);
+            expect(manzana.length).toBe(2);
+        });
     });
 });
